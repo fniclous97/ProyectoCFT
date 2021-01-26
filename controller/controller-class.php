59ff8fba67be215cec8ad29d41bc65541444bf6c
fniclous97 <fniclous@gmail.com>
2021-01-26 18:24:41 -0400

    Mvc actualizado con funciones CRUD

--- controller/controller-class.php
+++ controller/controller-class.php
@@ -15,5 +15,64 @@ class Controlador{
 		include $res;
 
 	}
+
+	//consultar  database
+	public function consultarConexion () {
+
+		$db_data= require_once 'config/db-cfg.php';
+		
+		$conn = new mysqli($db_data['host'], $db_data['user'], $db_data['pass'], $db_data['database']);
+
+		if($conn){
+			
+			echo "<h1>CONEXION EXITOSA</h1>";
+			
+		}else{
+			echo "<h1>CONEXION RECHAZADA</h1>";
+
+		}
+		
+	}
+
+	public function crearBaseDatos ($dbname) {
+
+		$db_data= require_once 'config/db-cfg.php';
+		
+		$conn = new mysqli($db_data['host'], $db_data['user'], $db_data['pass'], $db_data['database']);
+
+		if($conn){
+			
+			echo "<h1>CONEXION EXITOSA</h1>";
+			$query = printf("CREATE DATABASE `%d`",$dbname);
+			mysqli_query($conn, $query);
+			echo "<h2>BASE DE DATOS CREADA CON EXITO.</h2>";
+
+
+			
+		}else{
+			echo "<h1>CONEXION RECHAZADA</h1>";
+
+		}
+		
+	}
+
+	public function crearUsuario($rut, $pass, $privi){
+		$db_data= require_once 'config/db-cfg.php';
+		$conn = new mysqli($db_data['host'], $db_data['user'], $db_data['pass'], $db_data['database']);
+		if($conn){
+			
+			try {
+				$query = printf("INSERT INTO `usuarios`(`rut`, `pass`, `privilegio`) VALUES ('%d', '%d', '%d');", $rut, $pas, $privi);
+				mysqli_query($conn, $query);
+			} catch (Exception $err) {
+				echo $err;
+			}
+			
+			
+		}else{
+			echo "<h1>CONEXION RECHAZADA</h1>";
+
+		}
+	}
 }
 ?>
--- view/modulos/login.php
+++ view/modulos/login.php
@@ -1,1 +1,23 @@
 <h1>ACCEDER</h1>
+<div>
+    <form action="index.php"> 
+        <hr>
+        <label>Rut</label> <br>
+        <input type="text" name="usuario"><br>
+        <label>Contraseña</label> <br>
+        <input type="password" name="usuario">
+        <br><br>
+        <input type="submit" value="Iniciar sesion">
+        <input type="reset" value="Limpiar" class="btn btn-primary"><hr>
+        
+    </form>
+    <a href="#" class="btn btn-secondary">Registrar</a>
+    <a href="#" class="btn btn-secondary">Recuperas clave</a>
+    <div>
+
+        <?php
+            $var = new Controlador;
+            $var -> consultarConexion();
+        ?>
+    </div>
+</div>
